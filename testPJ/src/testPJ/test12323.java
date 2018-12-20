9b60ef7b4d24543c178133e7fe6d92fea7744097
goott6-2 <goott6-2@goott6-2-PC>
2018-12-20 12:24:04 +0900

    asdasdas

--- testPJ/src/testPJ/test12323.java
+++ testPJ/src/testPJ/test12323.java
@@ -3,8 +3,9 @@ package testPJ;
 public class test12323 {
 
 	public static void main(String[] args) {
-		// TODO 자동 생성된 메소드 스텁
+		// TODO 자동 생성된 메소드 스텁asdasdasdadadsadsadssdsadsd
 		System.out.println("dd");
+		System.out.println("Sadadsa");
 	}
 
 }
